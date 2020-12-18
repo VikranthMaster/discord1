da1132ea99d160f7086d2ca95cf0d6eaf6ae30c3
VikranthMaster <69100217+VikranthMaster@users.noreply.github.com>
2020-12-18 17:16:33 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,61 @@ import discord
 import json
 from discord.ext import commands
 import youtube_dl
+import os
+from random import choice
+from discord.voice_client import VoiceClient
+from random import choice
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+
+        self.data = data
+
+        self.title = data.get('title')
+        self.url = data.get('url')
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+
+        filename = data['url'] if stream else ytdl.prepare_filename(data)
+        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
 
 client = commands.Bot(command_prefix=">",intents=discord.Intents.all())
-
+queue = []
 
 @client.event
 async def on_ready():
     print(f"we have logged in as {client.user}")
-
+    # channel = discord.utils.get(member.guild.channels, name="general")
 
 @client.event
 async def on_raw_reaction_add(payload):
@@ -62,9 +109,37 @@ async def poll(ctx, *, message):
     await msg.add_reaction('👎')
 
 
+@client.event
+async def on_raw_reaction_add(payload):
+
+    if payload.member.bot:
+        pass
+
+    else:
+        with open('reactrole.json') as react_file:
+            data = json.load(react_file)
+            for x in data:
+                if x['emoji'] == payload.emoji.id:
+                    role = discord.utils.get(client.get_guild(
+                        payload.guild_id).roles, id=x['role_id'])
+
+                    await payload.member.add_roles(role)
+
+@client.event
+async def on_raw_reaction_remove(payload):
+
+    with open('reactrole.json') as react_file:
+        data = json.load(react_file)
+        for x in data:
+            if x['emoji'] == payload.emoji.id:
+                role = discord.utils.get(client.get_guild(
+                    payload.guild_id).roles, id=x['role_id'])
+
+                
+                await client.get_guild(payload.guild_id).get_member(payload.user_id).remove_roles(role)
+                    
 @client.command()
-@commands.has_permissions(administrator=True, manage_roles=True)
-async def reactrole(ctx, emoji, role: discord.Role, *, message):
+async def reactrole(ctx, emoji: discord.Emoji, role: discord.Role, *, message):
 
     emb = discord.Embed(description=message)
     msg = await ctx.channel.send(embed=emb)
@@ -75,7 +150,7 @@ async def reactrole(ctx, emoji, role: discord.Role, *, message):
 
         new_react_role = {'role_name': role.name, 
         'role_id': role.id,
-        'emoji': emoji,
+        'emoji': emoji.id,
         'message_id': msg.id}
 
         data.append(new_react_role)
@@ -83,10 +158,30 @@ async def reactrole(ctx, emoji, role: discord.Role, *, message):
     with open('reactrole.json', 'w') as f:
         json.dump(data, f, indent=4)
 
-@client.command()
-async def play(ctx, url : str):
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name="General")
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    await voiceChannel.connect()
+
+@client.command(name="play", help="This command plays songs.")
+async def play(ctx, url):
+    if not ctx.message.author.voice:
+        await ctx.send("You are not connected to a voice channel")
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    
+    await channel.connect()
+
+    server = ctx.message.guild
+    voice_channel = server.voice_client
+
+    async with ctx.typing():
+        player = await YTDLSource.from_url(url, loop=client.loop)
+        voice_channel.play(player, after=lambda e: print("Player error: %s" %e)if e else None)
+
+    await ctx.send(f"Now playing {player.title}")
+
+@client.command(name="stop", help="This command stops the song ")
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    await voice_client.disconnect()
+
 
 client.run('TOKEN')
